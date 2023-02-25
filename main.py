0ef4993125b71c1daf2e034f551d3ceb68639bd4
Aditya Shah <shahaditya@outlook.com>
2023-02-24 23:40:22 -0800

    hm

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello World!")
+from flask import Flask, render_template, jsonify, request
+import json 
+
+app = Flask(__name__)
+
+@app.route('/login')
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=3000, debug=True)
 
